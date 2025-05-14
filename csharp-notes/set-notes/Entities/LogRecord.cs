bdb594da37048b43a3d95b8b0038ef34b6973572
Paola Caroline <paolacaroline-sv@outlook.com>
2025-05-14 12:23:04 -0300

    feat: add set-notes

---
+++ csharp-notes/set-notes/Entities/LogRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set_notes.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+    }
+}
---
+++ csharp-notes/set-notes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using set_notes.Entities;
+
+namespace set_notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine(); 
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+            
+            try
+            {
+                using (StreamReader sr = File.OpenText(path)) //exemple format: 'amanda 2020-08-26T20:45:08'
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split();                       
+                        set.Add(new LogRecord() { Username = line[0], Instant = DateTime.Parse(line[1]) });
+                    }
+                    Console.WriteLine("Total acess: " + set.Count);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
