a805e5e33b995c0bdd517bd136e23131f98613eb
Paola Caroline <paolacaroline-sv@outlook.com>
2025-05-14 13:26:56 -0300

    feat: add new exercise file about sets

---
+++ csharp-exercise/StudentsPortalExercise/Entities/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsPortalExercise.Entities
+{
+    internal class Student
+    {
+        public int ID { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Student student &&
+                   ID == student.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID);
+        }
+    }
+}
---
+++ csharp-exercise/StudentsPortalExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using StudentsPortalExercise.Entities;
+
+namespace StudentsPortalExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> studentsA = new HashSet<Student>();
+            HashSet<Student> studentsB = new HashSet<Student>();
+            HashSet<Student> studentsC = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Student ID: ");
+                studentsA.Add(new Student() { ID = int.Parse(Console.ReadLine()) });
+            }
+
+            Console.Write("How many students for course B? ");
+             n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Student ID: ");
+                studentsB.Add(new Student() { ID = int.Parse(Console.ReadLine()) });
+            }
+
+            Console.Write("How many students for course C? ");
+             n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Student ID: ");
+                studentsC.Add(new Student() { ID = int.Parse(Console.ReadLine()) });
+            }
+
+            HashSet<Student> allStudent = new HashSet<Student>(studentsA);
+            allStudent.UnionWith(studentsB);
+            allStudent.UnionWith(studentsC);
+            Console.WriteLine("Total students: " + allStudent.Count);
+        }
+    }
+}
