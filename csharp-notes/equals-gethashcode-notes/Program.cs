3571cdd75922080c482f4db3a61ec8ae6ad0696d
Paola Caroline <paolacaroline-sv@outlook.com>
2025-05-14 11:13:59 -0300

    feat: add GetHashCode and Equals notes

---
+++ csharp-notes/equals-gethashcode-notes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using equals_gethashcode_notes.Entities;
+
+namespace equals_gethashcode_notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail"};
+            Client b = new Client { Name = "Alex", Email = "alex@gmail" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            Console.WriteLine(a == b);
+        }
+    }
+}
