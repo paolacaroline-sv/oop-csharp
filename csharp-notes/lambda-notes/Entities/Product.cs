bbb1c9b6df5af400d41a6a7d34e5e5b3138291e6
Paola Caroline <paolacaroline-sv@outlook.com>
2025-05-15 13:10:00 -0300

    feat: add lambda-notes

---
+++ csharp-notes/lambda-notes/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace lambda_notes.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+    }
+}
---
+++ csharp-notes/lambda-notes/Program.cs
@@ -0,0 +1,54 @@
+﻿using lambda_notes.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace lambda_notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter full file path: ");
+            string path = @"C:\temp\in.txt"; //exemple: Tv,900.00
+            Console.WriteLine("** ok!");
+
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(",");
+                        string name = line[0];
+                        double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+                        products.Add(new Product(name, price));
+                    }
+                }
+
+                var averagePrices =
+                (from p in products
+                 select p.Price).Average();
+                Console.WriteLine("Avarage price products: " + averagePrices.ToString("F2", CultureInfo.InvariantCulture));
+
+                var namesProd =
+                    from p in products
+                    where p.Price < averagePrices
+                    orderby p.Price descending
+                    select p.Name;
+                foreach (var name in namesProd)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Ops!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
